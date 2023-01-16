194ef7a89f82b7be3ca20acaf5d1084801543fa2
Daniele R.C <darthreca@gmail.com>
2023-01-16 12:52:12 +0100

    :new: Added requirements and using main

--- main.py
+++ main.py
@@ -1,54 +1,85 @@
+import comet_ml
 import tianshou as ts
 import gym
 from tianshou.utils.net.common import Net
 import tianshou.utils.net.discrete as discrete
 import torch
+from tianshou.utils import WandbLogger, TensorboardLogger
 from torch.utils.tensorboard import SummaryWriter
-from tianshou.utils import WandbLogger
 
+POLICY_CONFIG = {
+    "is_double": False,
+    "discount_factor": 0.99,
+    "target_update_freq": 100,
+}
 
-logger = WandbLogger()
-writer = SummaryWriter("logs/lunar-lander")
-logger.load(writer)
+def train():
+    optim_config = {
+        "lr": 0.001
+    }
 
-env = gym.make("LunarLander-v2")
-train_env = ts.env.DummyVectorEnv([lambda: gym.make("LunarLander-v2") for _ in range(10)])
-test_env = ts.env.DummyVectorEnv([lambda: gym.make("LunarLander-v2") for _ in range(2)])
+    env = gym.make("LunarLander-v2")
+    train_env = ts.env.DummyVectorEnv([lambda: gym.make("LunarLander-v2") for _ in range(10)])
+    test_env = ts.env.DummyVectorEnv([lambda: gym.make("LunarLander-v2") for _ in range(2)])
 
-state_shape = env.observation_space.shape or env.observation_space.n
-actions_n = env.action_space.shape or env.action_space.n
+    state_shape = env.observation_space.shape or env.observation_space.n
+    actions_n = env.action_space.shape or env.action_space.n
 
-actor = discrete.Actor(Net(state_shape, hidden_sizes=[32, 32]), action_shape=actions_n)
-actor_optim = torch.optim.Adam(actor.parameters(), lr=3e-4)
+    actor = Net(state_shape, actions_n, hidden_sizes=[512, 256])
+    actor_optim = torch.optim.AdamW(actor.parameters(), **optim_config)
 
-critic_1 = discrete.Critic(Net(state_shape, actions_n, hidden_sizes=[32, 32]), last_size=actions_n)
-critic_1_optim = torch.optim.Adam(critic_1.parameters(), lr=3e-4)
+    dqn = ts.policy.DQNPolicy(
+        actor, actor_optim,
+        **POLICY_CONFIG
+    )
 
-critic_2 = discrete.Critic(Net(state_shape, actions_n, hidden_sizes=[32, 32]), last_size=actions_n)
-critic_2_optim = torch.optim.Adam(critic_2.parameters(), lr=3e-4)
+    train_collector = ts.data.Collector(dqn, train_env, ts.data.VectorReplayBuffer(500000, 10), exploration_noise=False)
+    test_collector = ts.data.Collector(dqn, test_env, exploration_noise=False)
 
-dqn = ts.policy.DiscreteSACPolicy(
-    actor, actor_optim,
-    critic_1, critic_1_optim,
-    critic_2, critic_2_optim,
-    tau=0.005, gamma=0.9,
-)
+    # Comet
+    experiment = comet_ml.Experiment(api_key="sB2qT71Uklji1EGNlkZ2WhuzL")
+    experiment.set_name(f"{dqn.__class__.__name__}_LunarLander-v2")
+    experiment.log_parameters({**POLICY_CONFIG, **optim_config})
+    experiment.set_model_graph(dqn)
+    # Tensorboard
+    writer = SummaryWriter(f"./logs/lunar-lander/")
+    writer.add_text("hparams", str({**POLICY_CONFIG, **optim_config}))
+    logger = TensorboardLogger(writer, train_interval=10, update_interval=10, test_interval=1)
+    # Training
+    result = ts.trainer.offpolicy_trainer(
+        dqn, train_collector, test_collector,
+        max_epoch=1000,
+        batch_size=128,
+        step_per_epoch=1000, step_per_collect=10, update_per_step=1,
+        episode_per_test=5,
+        train_fn=lambda epoch, step: dqn.set_eps(1.0 * (1.0 - epoch/1000)),
+        stop_fn=lambda rew: rew >= env.spec.reward_threshold,
+        save_best_fn=lambda policy: torch.save(policy.state_dict(), f"checkpoints/dqn.pth"),
+        logger=logger
+    )
+    experiment.log_model("Best model", "checkpoints/dqn.pth")
 
-train_collector = ts.data.Collector(dqn, train_env, ts.data.VectorReplayBuffer(20000, 10), exploration_noise=True)
-test_collector = ts.data.Collector(dqn, test_env, exploration_noise=True)
+    train_env.close()
+    test_env.close()
+    env.close()
 
+def test():
+    env = gym.make("LunarLander-v2", render_mode="human")
 
-def train_fn(epoch, step):
-    pass
+    state_shape = env.observation_space.shape or env.observation_space.n
+    actions_n = env.action_space.shape or env.action_space.n
+    env = ts.env.DummyVectorEnv([lambda : env])
+    env.reset()
 
+    actor = Net(state_shape,  actions_n, hidden_sizes=[512, 256])
+    optim = torch.optim.AdamW(actor.parameters(), lr=0.001)
+    policy = ts.policy.DQNPolicy(actor, optim, **POLICY_CONFIG)
+    policy.load_state_dict(torch.load("checkpoints/dqn.pth"))
+    # Testing
+    policy.eval()
+    collector = ts.data.Collector(policy, env, exploration_noise=True)
+    collector.collect(n_episode=10, render=1/60)
+    env.close()
 
-ts.trainer.offpolicy_trainer(
-    dqn, train_collector, test_collector, max_epoch=10, batch_size=128,
-    step_per_epoch=10000, step_per_collect=10,
-    train_fn=train_fn,
-    episode_per_test=10, stop_fn=lambda rew: rew >= env.spec.reward_threshold,
-    logger=logger
-)
-
-train_env.close()
-test_env.close()
+if __name__ == "__main__":
+    test()
